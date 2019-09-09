d8239aaaff72a2dd4cc5d86de12c7dba9248b10e
Ana Karenina <louiseanni2011@gmail.com>
2019-09-08 21:23:01 -0700

    submission

---
+++ Homework Submission/static/js/app.js
@@ -0,0 +1,44 @@
+// from data.js
+var tableData = data;
+
+// Create variables.
+var button = d3.select("#filter-btn")
+var tblbody = d3.select('tbody');
+
+// Create table with data from data.js
+function createTable(data){
+    tblbody.html('');
+    data.forEach((row)=>{
+        var tblrow = tblbody.append("tr");
+        Object.values(row).forEach((val) => {
+            var cell = tblrow.append("td");
+              cell.text(val);
+            }
+          );
+    })
+}
+// Call the function to create table when page loads 
+createTable(tableData);
+
+// Create event handler function that points to the element, grabs the input value and creates 
+// array with the filtered data matching the input value. If there is no data, it will print "There are no matching results." 
+function handleClick () {
+    
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputDate = inputElement.property("value");
+    var filteredData = tableData.filter(tableData=>tableData.datetime ===inputDate);
+    // Print filteredData to check values are correct.
+    console.log(filteredData)
+    // Print length to check the number of rows of data found that matches inputDate.
+    console.log(filteredData.length)
+    // Clear the tblbody before rendering result
+    tblbody.html("");
+        if (filteredData.length !== 0){
+       createTable(filteredData)
+    } else {tblbody.append("tr").append("td").text("There are no matching results.");}
+}
+
+//Add element and action to call the function when click event occurs. 
+button.on("click", handleClick);
+
